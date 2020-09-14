d8be5cbcc85c73f24a2cf8a2eadfc31a2b7eda0a
Mike Beautyfarmer <michaelschoenbauer@gmx.at>
2020-09-14 22:14:28 +0200

    second commit

--- secretnumber.py
+++ secretnumber.py
@@ -5,12 +5,16 @@ versuche = 0
 print("Errate die geheime Zahl zwischen 1 und 50.")
 
 while guess != secret:
+
     guess = int(input("Deine Zahl: "))
     versuche = versuche + 1
+
     if guess < secret:
         print("Diese Zahl war zu niedrig.")
+
     if guess > secret:
         print("Diese Zahl war zu hoch.")
 
-print("Sehr gut! Du hast die geheime Zahl erraten. Es ist die " + str(secret) + ".")
-print("Du hast " + str(versuche) + " Versuche gebraucht um die geheime Zahl zu entdecken!")
+print(f"Sehr gut! Du hast die geheime Zahl erraten. Es ist die  {secret}.")
+
+print(f"Du hast {versuche} Versuche gebraucht um die geheime Zahl zu entdecken!")
